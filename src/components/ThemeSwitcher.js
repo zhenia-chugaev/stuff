6d7c177d25ed3daf0f2564abe4bc07b7bd5dfe8e
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-10-19 12:49:04 +0300

    feat: add theme switcher behaviour

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -1,12 +1,21 @@
 import { Switch } from "antd";
 
-const ThemeSwitcher = () => (
-  <Switch
-    className="theme-switcher"
-    checkedChildren="Light"
-    unCheckedChildren="Dark"
-    defaultChecked={true}
-  />
-);
+const ThemeSwitcher = () => {
+  const toggleThemes = () => {
+    const app = document.body;
+    app.classList.toggle('dark-theme');
+    app.classList.toggle('light-theme');
+  };
+
+  return (
+    <Switch
+      className="theme-switcher"
+      checkedChildren="Light"
+      unCheckedChildren="Dark"
+      defaultChecked={true}
+      onChange={toggleThemes}
+    />
+  );
+};
 
 export default ThemeSwitcher;
