131050b3a002bed1e7c34f895e7e0dc85d269a38
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-10-19 19:16:08 +0300

    feat: create basic banner section markup

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,13 @@
-const Banner = () => (
-  <section className="">
+// import AuthenticationForm from './AuthenticationForm';
 
+const Banner = () => (
+  <section className="banner">
+    <h1 className="sr-only">
+      Collection Manager - your ultimate solution for managing favorite collections.
+    </h1>
+    <h2 className="heading heading_size_l text">Create, manage, share</h2>
+    <p className="heading heading_size_m text">everything from a single place</p>
+    {/* <AuthenticationForm /> */}
   </section>
 );
 
