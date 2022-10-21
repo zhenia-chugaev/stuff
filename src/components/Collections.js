27c56773b1a185a7334eca7a9141151ebb569e55
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-10-21 18:50:28 +0300

    feat: add collections list component

--- src/components/Collections.js
+++ src/components/Collections.js
@@ -1,6 +1,23 @@
+import { List } from 'antd';
+// import Collection from './Collection';
+
+const collections = [];
+
 const Collections = () => (
   <section className="section">
-
+    <h2 className="heading heading_size_s">Largest collections</h2>
+    <div className="section__inner">
+      <List
+        className="colls-list"
+        grid={{ gutter: 15, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 5 }}
+        dataSource={collections}
+        renderItem={(collection) => (
+          <List.Item>
+            {/* <Collection props={collection} /> */}
+          </List.Item>
+        )}
+      />
+    </div>
   </section>
 );
 
