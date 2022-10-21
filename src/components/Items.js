2ff3c33b3d5a7319695b6667e20c95876b937311
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-10-21 15:48:39 +0300

    feat: add items list component

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,6 +1,23 @@
+import { List } from 'antd';
+// import Item from './Item';
+
+const items = [];
+
 const Items = () => (
   <section className="section">
-
+    <h2 className="heading heading_size_s">Recently added</h2>
+    <div className="section__inner">
+      <List
+        className="items-list"
+        grid={{ gutter: 20, xs: 1, sm: 2, md: 3, }}
+        dataSource={items}
+        renderItem={(item) => (
+          <List.Item>
+            {/* <Item props={item} /> */}
+          </List.Item>
+        )}
+      />
+    </div>
   </section>
 );
 
