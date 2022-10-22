ae4d5edb891d6f64cf27309030872f66983397fa
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-10-22 20:52:54 +0300

    fix: add attributes to the search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,12 @@
 import { Input } from 'antd';
+
 const { Search } = Input;
 
 const SearchForm = () => (
-  <form className="search-form">
+  <form className="search-form" method="GET" action="/search">
     <Search
       className="search-form__input"
+      name="query"
       placeholder="books"
     />
   </form>
