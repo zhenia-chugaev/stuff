a999724daf8ad15727d13be8563314a041f0a7e6
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-10-22 23:33:42 +0300

    test: delete covered test

--- src/__tests__/Footer.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Footer from '../components/Footer';
-
-it('renders quote author', () => {
-  render(<Footer />);
-  const quoteAuthor = screen.getByText(/Albert Camus/i);
-  expect(quoteAuthor).toBeInTheDocument();
-});
