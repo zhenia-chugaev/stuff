59fe24920e9f7a5ee2e2da810b09b59cac241bde
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-10-28 18:56:05 +0300

    feat: add search results page

--- src/components/views/SearchResults.jsx
+++ src/components/views/SearchResults.jsx
@@ -1,11 +1,41 @@
-import { useSearchParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useSearchParams, Link } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import { List } from 'antd';
+import getData from '../../utils/fetch';
 
 const SearchResults = () => {
+  const [searchResults, setSearchResults] = useState([]);
   const [searchParams] = useSearchParams();
   const query = searchParams.get('query');
 
+  useEffect(() => {
+    getData(`/.netlify/functions/search?query=${query}`, setSearchResults);
+  }, [query]);
+
+  const listItems = searchResults.map((item, i) => {
+    const orderNumber = i + 1;
+    const path = `/${item.type}/${item.id}`;
+    return (
+      <List.Item className="search-results__item">
+        {orderNumber}. {item.name} <Link to={path}>{path}</Link>
+      </List.Item>
+    )
+  });
+
   return (
-    <div>Search results for {query}</div>
+    <section className="section search-results">
+      <h1 className="sr-only">Search results</h1>
+      <p className="search-results__message">
+        <FormattedMessage id="main.search-results.message" />: {searchResults.length}
+      </p>
+      <List
+        className="search-results__list"
+        dataSource={listItems}
+        renderItem={(item) => item}
+        size="large"
+      />
+    </section>
   );
 };
 
