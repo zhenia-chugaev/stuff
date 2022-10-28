bb352ac7461bf31cad47fe826945bb9bb0190fb4
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-10-28 22:33:20 +0300

    refactor: change search form implementation

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,28 +1,32 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Input } from 'antd';
+import { Input, Button } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 
 const SearchForm = () => {
   const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
-  const handleSearch = () => {
-    if (query) {
-      navigate(`/search?query=${query}`);
-      setQuery('');
-    }
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search?query=${query}`);
+    setQuery('');
   };
 
   return (
-    <form className="search-form" method="get" action="/search" onSubmit={(e) => e.preventDefault()}>
-      <Input.Search
+    <form className="search-form" method="get" action="/search" onSubmit={handleFormSubmit}>
+    <Input.Group>
+      <Input
         className="search-form__input"
         name="query"
         placeholder="books"
         value={query}
+        required
+        style={{ width: 'calc(100% - 2rem)' }}
         onChange={(e) => setQuery(e.target.value)}
-        onSearch={handleSearch}
       />
+      <Button className="search-form__button" icon={<SearchOutlined />} htmlType="submit" />
+    </Input.Group>
     </form>
   );
 };
