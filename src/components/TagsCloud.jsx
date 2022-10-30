322761a940f00664d442fddbcd18e5863e242a9e
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-10-30 13:02:53 +0300

    refactor: abstract callback function

--- src/components/TagsCloud.jsx
+++ src/components/TagsCloud.jsx
@@ -7,14 +7,16 @@ import getData from '../utils/fetch';
 const TagsCloud = () => {
   const [tags, setTags] = useState([]);
 
+  const setUniqueTags = (items) => {
+    const tags = items
+      .flatMap((item) => item.tags)
+      .map((tag) => tag.toLowerCase());
+    const uniqueTags = uniq(tags);
+    setTags(uniqueTags);
+  };
+
   useEffect(() => {
-    getData('/.netlify/functions/index?from=items', (items) => {
-      const tags = items
-        .flatMap((item) => item.tags)
-        .map((tag) => tag.toLowerCase());
-      const uniqueTags = uniq(tags);
-      setTags(uniqueTags);
-    })
+    getData('/.netlify/functions/index?from=items', setUniqueTags);
   }, []);
 
   const tagsList = shuffle(tags).map((tag, i) => (
