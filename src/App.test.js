0f282b74988024fdf17aea038ebeb3d50209cb6b
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-12-13 17:06:32 +0300

    test: change existing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world string', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const greeting = screen.getByText(/hello, world/i);
+  expect(greeting).toBeInTheDocument();
 });
