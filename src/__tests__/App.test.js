ecbc578cfb0b4e73eb389c66c60577b85f8332f9
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-12-14 12:55:44 +0300

    chore: change app's file structure

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders hello world string', () => {
   render(<App />);
-  const greeting = screen.getByText(/hello, world/i);
+  const greeting = screen.getByText(/hello/i);
   expect(greeting).toBeInTheDocument();
 });
