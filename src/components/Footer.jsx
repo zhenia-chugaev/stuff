6179b0698f8da5c2e4de4fe71423068b4dee6e29
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-12-23 17:30:51 +0300

    feat: add link to github repository

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage } from 'react-intl';
+import { GithubFilled } from '@ant-design/icons';
 
 const Footer = () => (
   <footer className="footer">
     <div className="container">
-      <p className="footer__quote">
-        <FormattedMessage id="footer.quote" />
-      </p>
+      <div className="footer__inner">
+        <a href="https://github.com/zhenia-chugaev/stuff" target="blank">
+          <GithubFilled className="footer__icon"/>
+        </a>
+        <p className="footer__quote">
+          <FormattedMessage id="footer.quote" />
+        </p>
+      </div>
     </div>
   </footer>
 );
